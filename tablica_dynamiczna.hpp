1125491e9f6165364ae11d0ea7ff3fdbece83fc4
Kuba <kubaszac089@gmail.com>
2025-04-09 16:24:33 +0200

    dodano plik tablica_dynamiczna.hpp

---
+++ tablica_dynamiczna.hpp
@@ -0,0 +1,27 @@
+#ifndef tablica_dynamiczna_hpp
+#define tablica_dynamiczna_hpp
+
+class Tablica_dynamiczna{
+    private:
+        int* tab;
+        int size;
+        // aktualna ilosc elementow
+        int actual;
+    public: 
+        // konstruktor i desktruktor
+        Tablica_dynamiczna(int size);
+        ~Tablica_dynamiczna();
+
+        int resize();
+    
+        void add_beg(int element);
+        void add_end(int element);
+        void add(int element, int position);
+
+        void del_beg();
+        void del_end();
+        void del(int position);
+
+        bool find(int x) const;
+};
+#endif
