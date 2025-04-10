9fc08b6f025798708a486049dd46f86f6a5fba15
Kuba <kubaszac089@gmail.com>
2025-04-10 19:49:42 +0200

    dodano plik tablica dynamiczna

---
+++ dynamiczna.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+#include "tablica_dynamiczna.hpp"
+
+using namespace std;
+
+Tablica_dynamiczna::Tablica_dynamiczna(int s) {actual=0; tab=new int(size); size = s;}
+Tablica_dynamiczna::~Tablica_dynamiczna() {delete[] tab;}
+
+int Tablica_dynamiczna::resize() {
+    int n_size = 2*size;
+    int* n_tab = new int[n_size];
+
+    for (int i=0; i<size; i++) {
+        n_tab[i]=tab[i];
+    }
+    delete[] tab;
+    tab = n_tab;
+    size = n_size;
+    return size;
+}
+
+void Tablica_dynamiczna::add_beg(int element) {
+    if(actual==size){resize();}
+    for(int i = actual - 1;i>=0;i--){ 
+        tab[i+1]=tab[i];
+    }
+    tab[0]=element;
+    actual++;
+}
+
+void Tablica_dynamiczna::add_end(int element) {
+    if(actual==size){resize();}
+    tab[actual]=element;
+    actual++;
+}
+
+
+void Tablica_dynamiczna::add(int element, int position){
+    position--;
+    if(actual==size){resize();}
+    for(int i=actual; i>=position; i--){
+        tab[i+1]=tab[i];
+    }
+    tab[position]=element;
+    actual++;
+    position++;
+}
+
+void Tablica_dynamiczna::del_beg(){
+    for(int i = 0; i < actual-1;i++){
+        tab[i]=tab[i+1];
+    }
+    actual--;
+}
+
+void Tablica_dynamiczna::del_end(){
+    tab[actual] = 0;
+    actual--;
+}
+
+void Tablica_dynamiczna::del(int position) {
+    position--;
+    for(int i = position; i<=actual; i++) {
+        tab[i]=tab[i+1];
+    }
+    actual--;
+    position++;
+}
+bool Tablica_dynamiczna::find(int x) const{
+    for(int i = 0; i<actual; i++) {
+        if(tab[i]==x){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
